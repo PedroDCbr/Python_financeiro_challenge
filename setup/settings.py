83350f6428c32ce77e80514e8dfd1f80a1331ef2
Pedro Duim Carvalho <pedro.duim.carvalho@gmail.com>
2022-08-28 16:47:50 -0300

    Iniciando deploy no Heroku

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +133,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ],
 }
+
+django_heroku.settings(locals())
